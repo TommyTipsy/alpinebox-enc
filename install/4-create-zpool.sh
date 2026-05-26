c24b6a5555633a16bf8344e46c4309ef5444f47a
TommyTipsy <timmytong@anche.no>
2026-05-25 19:26:00 -0600

    Update 4-create-zpool.sh

--- install/4-create-zpool.sh
+++ install/4-create-zpool.sh
@@ -9,7 +9,29 @@ set -e
 source config
 echo "ALPINEBOX: Creating zpool on $INSTALL_ZPOOL_DEV"
 
+printf "ZFS Passphrase: "
+stty -echo
+read -r ZFS_PASS
+stty echo
+printf "\n"
+
+printf "Confirm ZFS Passphrase: "
+stty -echo
+read -r ZFS_PASS2
+stty echo
+printf "\n"
+
+if 
+[ "$ZFS_PASS" != "$ZFS_PASS2" ] || 
+[ -z "$ZFS_PASS" ]; then
+    echo "ERROR: Passphrases do not match or are empty. Aborting."
+    exit 1
+fi
+
 zgenhostid -f
+
+printf "%s" "$ZFS_PASS" > /etc/zfs/zroot.key
+chmod 000 /etc/zfs/zroot.key
 
 zpool create \
     -f \
@@ -18,6 +40,9 @@ zpool create \
     -O mountpoint=none \
     -O acltype=posixacl \
     -O compression=on \
+    -O encryption=aes-256-gcm \
+    -O keylocation=file:///etc/zfs/zroot.key \
+    -O keyformat=passphrase \
     -o autoexpand=on \
     -O dnodesize=auto \
     -O normalization=formD \
